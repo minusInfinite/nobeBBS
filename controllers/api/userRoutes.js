3e6579b6cc2a3ea79f1d4e8a64b840e910fd4462
Riley Lum <rileylum@adam.com.au>
2021-09-09 20:04:35 +0930

    add login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -20,4 +20,30 @@ router.post('/new', async (req, res) => {
     }
 })
 
+// LOGIN EXISTING USER
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { username: req.body.username } });
+
+        if (!userData) {
+            return res.status(400).json({ message: "Incorrect email or password, please try again" })
+        }
+
+        const validPassword = await userData.validatePassword(req.body.password);
+
+        if (!validPassword) {
+            return res.status(400).json({ message: "Incorrect email or password, please try again" })
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json({ user: userData, message: "You are now logged in!" });
+        })
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
 module.exports = router;
