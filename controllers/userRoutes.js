903c0ce0bc9329f60eb9132103f9021ec365ed49
Riley Lum <rileylum@adam.com.au>
2021-09-09 20:14:49 +0930

    add logout route

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -4,4 +4,10 @@ router.get('/', (req, res) => {
     res.send('HELLO WORLD')
 })
 
+// LOGOUT ROUTE i.e. remove session
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+})
+
 module.exports = router;
