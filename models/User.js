f3432423cdbfc7fdb848fb007a0b70fc1cd8bb97
Riley Lum <rileylum@adam.com.au>
2021-09-09 19:40:27 +0930

    add user model

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const {Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    validatePassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+            unique: true
+        },
+        username: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        },
+        is_admin: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        last_login: {
+            type: DataTypes.DATE
+        },
+        avatar: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 12);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 12);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
