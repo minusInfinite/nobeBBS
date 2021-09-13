5cbb7eef53f6717035c6871c6d6400c421351264
Riley Lum <rileylum@adam.com.au>
2021-09-13 20:32:07 +0930

    add login form

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -4,6 +4,10 @@ router.get('/', (req, res) => {
     res.send('HELLO WORLD')
 })
 
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+
 // LOGOUT ROUTE i.e. remove session
 router.get('/logout', (req, res) => {
     req.session.destroy();
