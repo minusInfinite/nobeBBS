8f45606536530124f9a6ba48dafa783a361ae7fc
Blake Schumann <blakeschumann15@gmail.com>
2021-09-16 18:59:01 +0930

    a comment can only belong to one user

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Topic = require('./Topic');
 const Post = require('./Post');
+const Comment =require('./Comment')
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
@@ -8,6 +9,10 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
