1201718f6a37c64a561f0cbf8bdde06ee4425311
minusInfinite <ashley@minusinfinite.net>
2021-09-19 23:05:43 +0930

    Server Reset

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(routes)
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () =>
         console.log(`Server Runnning http://localhost:${PORT}`)
     )
