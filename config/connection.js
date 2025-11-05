e4a6dd0addcbf0a21c55f56d60ae5cd6f5beda02
minusInfinite <ashley@minusinfinite.net>
2025-11-06 00:24:19 +1030

    typo for in memory db

--- config/connection.js
+++ config/connection.js
@@ -6,16 +6,7 @@ let sequelize
 if (process.env.DATABASE_URL || process.env.DB_URL) {
     sequelize = new Sequelize(process.env.DATABASE_URL || process.env.DB_URL)
 } else {
-    sequelize = new Sequelize(
-        process.env.DBNAME,
-        process.env.DBUSER,
-        process.env.DBPASS,
-        {
-            host: "localhost",
-            dialect: "postgres",
-            port: 5432,
-        }
-    )
+    sequelize = new Sequelize('sqlite::memory')
 }
 
 module.exports = sequelize
